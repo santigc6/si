28f07aedaada02ba500a3a15101eea087c547656
santigc6 <santi.4_1997@hotmail.com>
2018-10-08 01:28:33 +0200

    loads json

--- P2/1401_Fernandez_Gonzalez-Carvajal/webflix.py
+++ P2/1401_Fernandez_Gonzalez-Carvajal/webflix.py
@@ -1,4 +1,6 @@
-from flask import Flask
+from flask import Flask, render_template, json, url_for
+import os
+import sys
 
 app = Flask(__name__)
 
@@ -6,12 +8,17 @@ app = Flask(__name__)
 @app.route('/index.html')
 def index():
   try:
-    json_data = json.open('/json/catalogo.json')
-  except:
+    SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
+    json_url = os.path.join(SITE_ROOT, "json", "catalogo.json")
+    file_json = open(json_url)
+    json_data = json.load(file_json)
+  except IOError:
     return "<h1>There was a problem loading the catalogue</h1>"
+  
+  file_json.close()
   
   return "<h1>Hello There</h1>"
   
   
 if __name__ == '__main__':
-  app.run(host='0.0.0.0')
+  app.run(host='0.0.0.0', debug=True)
