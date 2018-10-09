8cdcd46d8f79453ef30da97607e1883845a4bd05
santigc6 <santi.4_1997@hotmail.com>
2018-10-09 23:20:29 +0200

    almost done; i am too clean

--- P2/1401_Fernandez_Gonzalez-Carvajal/webflix.py
+++ P2/1401_Fernandez_Gonzalez-Carvajal/webflix.py
@@ -17,8 +17,27 @@ def index():
   
   file_json.close()
   
-  return "<h1>Hello There</h1>"
+  return render_template('index.html', **json_data)
+
+@app.route('/details/<string:pelicula>')
+def details(pelicula):
+  try:
+    SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
+    json_url = os.path.join(SITE_ROOT, "json", "catalogo.json")
+    file_json = open(json_url)
+    json_data = json.load(file_json)
+  except IOError:
+    return "<h1>There was a problem loading the information of the film</h1>"
   
+  file_json.close()
+  
+  for peli in json_data['peliculas']:
+    if peli['titulo'] == pelicula:
+      context = dict(peli)
+      break
+      
+  return render_template('details.html', **context)  
+    
   
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
