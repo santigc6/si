41301f0aeb0279b2d0693c899baa3e296440211b
santigc6 <santigolol6@gmail.com>
2018-11-13 16:21:11 +0100

    getTopVentas

--- P3/getTopVentas.sql
+++ P3/getTopVentas.sql
@@ -1,8 +1,15 @@
-CREATE FUNCTION getTopVentas (anio)
-  RETURNS as $$
-DECLARE
-
+CREATE OR REPLACE FUNCTION getTopVentas (anio integer)
+  RETURNS TABLE (
+    year integer,
+    film character varying(255),
+    sales integer
+) as $$
 BEGIN
-
+  RETURN QUERY
+  SELECT DISTINCT imdb_movies.year, imdb_movies.movietitle, inventory.sales
+  FROM (inventory INNER JOIN products ON (inventory.prod_id=products.prod_id))
+  INNER JOIN imdb_movies ON (products.movieid=imdb_movies.movieid)
+  WHERE year >= anio
+  ORDER BY year;
 END;
 $$ LANGUAGE plpgsql;
