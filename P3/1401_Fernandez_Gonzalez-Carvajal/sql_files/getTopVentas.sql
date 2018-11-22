a4e9e4c2c55e7dde20d7f0692e776cad28aee7f5
santigc6 <santigolol6@gmail.com>
2018-11-22 19:03:51 +0100

    GOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOLLLLLLLLLLLLLDFINGER

--- P3/1401_Fernandez_Gonzalez-Carvajal/sql_files/getTopVentas.sql
+++ P3/1401_Fernandez_Gonzalez-Carvajal/sql_files/getTopVentas.sql
@@ -2,25 +2,24 @@ CREATE OR REPLACE FUNCTION getTopVentas (anio integer)
   RETURNS TABLE (
     year double precision,
     film character varying(255),
-    sales integer
+    sales bigint
 ) as $$
 BEGIN
   RETURN QUERY
   WITH tablaaux AS (
-  SELECT DATE_PART('year', orders.orderdate) as year, imdb_movies.movietitle, inventory.sales
+  SELECT DATE_PART('year', orders.orderdate) as anyo, imdb_movies.movietitle as titulo, SUM(orderdetail.quantity) as sales
   FROM orders INNER JOIN orderdetail ON (orders.orderid=orderdetail.orderid)
 	  INNER JOIN products ON (products.prod_id=orderdetail.prod_id)
-	  INNER JOIN inventory ON (products.prod_id=inventory.prod_id)
-	  INNER JOIN imdb_movies ON (imdb_movies.movieid=products.prod_id)
-  ORDER BY year
+	  INNER JOIN imdb_movies ON (imdb_movies.movieid=products.movieid)
+  GROUP BY imdb_movies.movietitle, anyo
   )
-  SELECT top.year, top.movietitle, top.sales
+  SELECT top.anyo, top.titulo, top.sales
   FROM (
 	  SELECT *, ROW_NUMBER() OVER (
-		  PARTITION BY tablaaux.year ORDER BY tablaaux.sales DESC) as rowid
+		  PARTITION BY tablaaux.anyo ORDER BY tablaaux.sales DESC) as rowid
 	  FROM tablaaux
   ) as top
-  WHERE rowid < 2 and top.year >= anio
-  ORDER BY top.year;
+  WHERE rowid < 2 and top.anyo >= anio
+  ORDER BY top.anyo;
 END;
 $$ LANGUAGE plpgsql;
