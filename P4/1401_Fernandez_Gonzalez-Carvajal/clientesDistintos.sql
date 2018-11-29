9eca9b7bef62d1b2bd14572a454375a50c06c07a
santigc6 <santigolol6@gmail.com>
2018-11-29 17:19:58 +0100

    HELLO THERE

--- P4/1401_Fernandez_Gonzalez-Carvajal/clientesDistintos.sql
+++ P4/1401_Fernandez_Gonzalez-Carvajal/clientesDistintos.sql
@@ -1,4 +1,4 @@
 -- Autores: Adrian Fernandez Amador y Santiago González- Carvajal Centenera
-SELECT DISTINCT customers.firstname
+SELECT COUNT(DISTINCT customers.customerid)
 FROM customers INNER JOIN orders ON (customers.customerid=orders.customerid)
 WHERE orders.totalamount > 100 AND TO_CHAR(orders.orderdate, 'YYYYMM') = '201504';
